fc218f788d1cb494c54522ce4e46f3fa8f19b656
Berken Utku Demirel <54396476+Berken-demirel@users.noreply.github.com>
2021-05-03 01:01:29 -0700

    Add files via upload

---
+++ ldpc_utils.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+UC Irvine CS274B
+
+Handout version:  Some unimplemented functionality indicated by TODO
+"""
+
+from factor_utils import SimpleFactorGraph, FastDiscreteFactor
+import numpy as np
+import copy
+
+
+def init_ldpc_graph(H, y, theta):
+    '''
+    INIT_LDPC_GRAPH - Creates a factor graph from a codebook H, noisy data y,
+    and binomial noise parameter 1-theta.
+    '''
+    G = SimpleFactorGraph()
+    z = channel_noise(y, 1-theta)
+    comparison = z == y
+    value = np.zeros((len(y),1))
+    counter = 0
+    for k in comparison:
+        if k:
+            value[counter,0] = theta
+        else:
+            value[counter,0] = 1-theta
+        counter = counter + 1
+    # Create variables for each column of H matrix
+    list_2_add_nodes_from = []
+    str1 = 'X'
+    for i in range(len(H[0])):
+        str_2_add = str1 + str(i)
+        list_2_add_nodes_from.append(str_2_add)
+    G.add_nodes_from(list_2_add_nodes_from)
+    # Create factors for each row of H matrix
+    factor_list = []
+    for i in range(len(H)):
+        list_2_factors = []
+        a = np.where(H[i, :] == 1)
+        for k in range(len(a[0])):
+            str_2_add = str1 + str(a[0][k])
+            list_2_factors.append(str_2_add)
+        probs = give_the_matrix(len(list_2_factors))
+        factor_list.append(FastDiscreteFactor(list_2_factors, [2] * len(list_2_factors),probs))
+        G.add_factors(factor_list[i])
+
+    G.add_nodes_from(factor_list)
+    for i in range(len(factor_list)):
+        a = np.where(H[i, :] == 1)
+        for k in range(len(a[0])):
+            str_2_add = str1 + str(a[0][k])
+            G.add_edges_from([(str_2_add, factor_list[i])])
+
+    factor_list_unary = []
+    # Create unary factors
+    for i in range(len(list_2_add_nodes_from)):
+        if y[i] == 1:
+            factor_list_unary.append(FastDiscreteFactor([list_2_add_nodes_from[i]], [2], np.array([1-value[i], value[i]])))
+            G.add_factors(FastDiscreteFactor([list_2_add_nodes_from[i]], [2], np.array([1-value[i], value[i]])))
+        else:
+            factor_list_unary.append(FastDiscreteFactor([list_2_add_nodes_from[i]], [2], np.array([value[i], 1-value[i]])))
+            G.add_factors(FastDiscreteFactor([list_2_add_nodes_from[i]], [2], np.array([value[i], 1-value[i]])))
+        G.add_edges_from([(list_2_add_nodes_from[i], factor_list_unary[i])])
+
+    G.add_nodes_from(factor_list_unary)
+
+    # TODO: FILL IN THE REST OF THE CODE!
+    # - For efficiency, define factors use the FastDiscreteFactor() class
+    #   (It has the same interface as DiscreteFactor(), but allows faster indexing)
+    # - Add factor/variable nodes using G.add_node (or G.add_nodes_from) and G.add_factors
+    # - Don't forget to add edges between variables and factors by calling G.add_edges_from 
+    # - Suggested variable names:  'X0', 'X1', 'X2' ...
+    
+    return G
+
+def give_the_matrix(length_of_variables):
+    limit = 2**length_of_variables
+    output = np.zeros((limit,1))
+    for k in range(limit):
+        a = f'{k:010b}'
+        if a.count('1') % 2 == 0:
+            output[k] = 1
+        else:
+            output[k] = 0
+    return output
+
+def estimate_code(marg):
+    '''
+    ESTIMATE_CODE - Estimates a codeword based on its marginal distributions.
+    This returns the most likely value of each bit under the marginals.
+
+    Parameters
+    ----------
+    marg : n-entry dictionary containing the marginal distribution of each variable
+
+    Returns
+    -------
+    decoded : n*1 numpy array containing the estimated codeword
+
+    '''
+    n = len(marg)
+    decoded = np.zeros([n,1])
+    # TODO: FILL IN THE REST OF THE CODE
+    for i in marg:
+        a = marg[i]
+        if a[0] > a[1]:
+            decoded[int(i[1:]),0] = 0
+        else:
+            decoded[int(i[1:]),0] = 1
+    return decoded
+        
+def channel_noise(x, noise):
+    '''
+    CHANNEL_NOISE - Simulates a noisy channel with specified noise level.
+
+    Parameters
+    ----------
+    x : n*1 numpy array of original data
+    noise : noise level
+
+    Returns
+    -------
+    y : n*1 numpy array of noisy data
+
+    '''
+    num_bits = len(x)
+    I_flip = np.where( np.random.rand(num_bits, 1) < noise)[0]
+    y = copy.deepcopy(x)
+    y[I_flip] = 1 - y[I_flip]
+    
+    return y
---
+++ question1.py
@@ -0,0 +1,292 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+HW2 Question 1
+
+UC Irvine CS274B
+"""
+from factor_utils import *
+from ldpc_utils import *
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+
+max_iters = 50
+conv_tol = 1e-6
+
+# QUESTION 1b #######################
+print('Question 1b')
+
+ldpc = sio.loadmat('ldpc36-128.mat')
+G = ldpc['G']
+H = ldpc['H']
+num_bits = np.size(G,0)
+noise = 0.04
+codeWord = np.zeros([num_bits, 1])
+
+# generate data
+noisyCodeWord = channel_noise(codeWord, noise)
+F = init_ldpc_graph( H, noisyCodeWord, 1-noise )
+actualError = np.sum(noisyCodeWord != codeWord) / len(noisyCodeWord)
+
+# Run Parallel Loopy BP
+
+nodeMarg_par = run_loopy_bp_parallel(F, max_iters, conv_tol)
+iters = len(nodeMarg_par)
+if iters < max_iters:
+    print('Parallel LBP converged in', iters, 'iterations.')
+else:
+    print('Parallel LBP did not converge. Terminated at', max_iters, 'iterations')
+
+p = [nodeMarg_par[-1][key][1] for key in nodeMarg_par[-1].keys()] # probabilities of xi = 1
+
+plt.figure(1)
+plt.plot(range(num_bits), p)
+plt.title('Bitwise Marginal Probability p(x_i=1)')
+plt.xlabel('Bit #')
+plt.ylabel('p(x_i=1)')
+plt.show()
+
+
+## QUESTION 1c #######################
+print('Question 1c')
+num_runs = 10
+noise = 0.06
+codeWord = np.zeros([num_bits, 1])
+
+# err_mat[i,j]: stores the hamming distance for run i at iteration j
+err_mat = np.zeros([num_runs, max_iters])
+
+longest = 0
+for m in range(num_runs):
+    print('## run ', m + 1, '##')
+    # generate data
+    noisyCodeWord = channel_noise(codeWord, noise)
+    F = init_ldpc_graph( H, noisyCodeWord, 1-noise )
+    actualError = np.sum(noisyCodeWord != codeWord) / len(noisyCodeWord)
+    print('actual error: ' , actualError)
+
+    # Run Parallel Loopy BP
+    nodeMarg_par = run_loopy_bp_parallel(F, max_iters, conv_tol)
+    iters = len(nodeMarg_par)
+    if iters < max_iters:
+        print('Parallel LBP converged in', iters, 'iterations.')
+    else:
+        print('Parallel LBP did not converge. Terminated at', max_iters, 'iterations')
+
+    # determine code
+    decoded = estimate_code( nodeMarg_par[-1] )
+    ham_dist = np.sum( decoded != codeWord )
+    print('Hamming Distance: ', ham_dist)
+    print('Error after correction ' , ham_dist/len(noisyCodeWord))
+
+    # store error at each iteration of this run
+    for it in range(iters):
+        decoded = estimate_code(nodeMarg_par[it])
+        err_mat[m,it] = np.sum(decoded != codeWord)
+    if iters < max_iters:
+        err_mat[m, iters:] = err_mat[m,iters-1]
+
+    # longest run (for plotting)
+    if longest < iters:
+        longest = iters
+    # plot
+    plt.figure(2)
+    for m in range(num_runs):
+        plt.plot(range(max_iters), err_mat[m])
+    plt.xlabel('Iteration #')
+    plt.ylabel('Hamming Distance')
+    plt.title('Channel Noise '+str(noise))
+
+## QUESTION 1d #######################
+print('Question 1d')
+num_runs = 10
+noise = 0.08
+codeWord = np.zeros([num_bits, 1])
+
+# err_mat[i,j]: stores the hamming distance for run i at iteration j
+err_mat = np.zeros([num_runs, max_iters])
+
+longest = 0
+for m in range(num_runs):
+    print('## run ', m + 1, '##')
+    # generate data
+    noisyCodeWord = channel_noise(codeWord, noise)
+    F = init_ldpc_graph(H, noisyCodeWord, 1 - noise)
+    actualError = np.sum(noisyCodeWord != codeWord) / len(noisyCodeWord)
+    print('actual error: ', actualError)
+
+    # Run Parallel Loopy BP
+    nodeMarg_par = run_loopy_bp_parallel(F, max_iters, conv_tol)
+    iters = len(nodeMarg_par)
+    if iters < max_iters:
+        print('Parallel LBP converged in', iters, 'iterations.')
+    else:
+        print('Parallel LBP did not converge. Terminated at', max_iters, 'iterations')
+
+    # determine code
+    decoded = estimate_code(nodeMarg_par[-1])
+    ham_dist = np.sum(decoded != codeWord)
+    print('Hamming Distance: ', ham_dist)
+    print('Error after correction ', ham_dist / len(noisyCodeWord))
+
+    # store error at each iteration of this run
+    for it in range(iters):
+        decoded = estimate_code(nodeMarg_par[it])
+        err_mat[m, it] = np.sum(decoded != codeWord)
+    if iters < max_iters:
+        err_mat[m, iters:] = err_mat[m, iters - 1]
+
+    # longest run (for plotting)
+    if longest < iters:
+        longest = iters
+    # plot
+    plt.figure(2)
+    for m in range(num_runs):
+        plt.plot(range(max_iters), err_mat[m])
+    plt.xlabel('Iteration #')
+    plt.ylabel('Hamming Distance')
+    plt.title('Channel Noise ' + str(noise))
+
+print('Question 1d-2')
+num_runs = 10
+noise = 0.1
+codeWord = np.zeros([num_bits, 1])
+
+# err_mat[i,j]: stores the hamming distance for run i at iteration j
+err_mat = np.zeros([num_runs, max_iters])
+
+longest = 0
+for m in range(num_runs):
+    print('## run ', m + 1, '##')
+    # generate data
+    noisyCodeWord = channel_noise(codeWord, noise)
+    F = init_ldpc_graph(H, noisyCodeWord, 1 - noise)
+    actualError = np.sum(noisyCodeWord != codeWord) / len(noisyCodeWord)
+    print('actual error: ', actualError)
+
+    # Run Parallel Loopy BP
+    nodeMarg_par = run_loopy_bp_parallel(F, max_iters, conv_tol)
+    iters = len(nodeMarg_par)
+    if iters < max_iters:
+        print('Parallel LBP converged in', iters, 'iterations.')
+    else:
+        print('Parallel LBP did not converge. Terminated at', max_iters, 'iterations')
+
+    # determine code
+    decoded = estimate_code(nodeMarg_par[-1])
+    ham_dist = np.sum(decoded != codeWord)
+    print('Hamming Distance: ', ham_dist)
+    print('Error after correction ', ham_dist / len(noisyCodeWord))
+
+    # store error at each iteration of this run
+    for it in range(iters):
+        decoded = estimate_code(nodeMarg_par[it])
+        err_mat[m, it] = np.sum(decoded != codeWord)
+    if iters < max_iters:
+        err_mat[m, iters:] = err_mat[m, iters - 1]
+
+    # longest run (for plotting)
+    if longest < iters:
+        longest = iters
+    # plot
+    plt.figure(2)
+    for m in range(num_runs):
+        plt.plot(range(max_iters), err_mat[m])
+    plt.xlabel('Iteration #')
+    plt.ylabel('Hamming Distance')
+    plt.title('Channel Noise ' + str(noise))
+
+# TODO: use modified code from 1c
+    
+## QUESTION 1e #######################
+
+print('Question 1e')
+ldpc = sio.loadmat('ldpc36-1600.mat')
+G = ldpc['G']
+H = ldpc['H']
+logo = ldpc['logo']
+num_bits = np.size(G,0)
+max_iters = 30
+
+# generate data
+noise = 0.06
+w = np.reshape(logo, [np.size(logo),1])
+codeWord = np.dot(G.astype(float), w) % 2
+noisyCodeWord = channel_noise(codeWord, noise)
+F = init_ldpc_graph( H, noisyCodeWord, 1-noise )
+actualError = np.sum(noisyCodeWord != codeWord) / len(noisyCodeWord)
+print('actual error: ' , actualError)
+nodeMarg_par = run_loopy_bp_parallel(F, max_iters, conv_tol)
+iters = len(nodeMarg_par)
+if iters < max_iters:
+    print('Parallel LBP converged in', iters, 'iterations.')
+else:
+    print('Parallel LBP did not converge. Terminated at', max_iters, 'iterations')
+    
+plotAtIters = [ 0, 1, 2, 3, 5, 10, 20, 30 ]
+plt.figure(5)
+fig,axs = plt.subplots(2,4,constrained_layout=True,subplot_kw={'xticks': [], 'yticks': []})
+for index in range(len(plotAtIters)):
+    i = plotAtIters[index]
+    
+    if i == 0:
+        thisX = noisyCodeWord
+    elif i >= len(nodeMarg_par):
+        thisX = estimate_code(nodeMarg_par[-1])
+    else:
+        thisX = estimate_code(nodeMarg_par[i])
+    
+    msg = np.reshape(thisX[:1600], [40,40])
+    par = np.reshape(thisX[1600:], [40,40])
+    im = np.vstack([msg,par])
+    axs.flat[index].imshow(im, cmap = 'gray')
+    axs.flat[index].set_xlabel('Iter '+str(i))
+    
+
+## QUESTION 1f #######################
+print('Question 1f')
+ldpc = sio.loadmat('ldpc36-1600.mat')
+G = ldpc['G']
+H = ldpc['H']
+logo = ldpc['logo']
+num_bits = np.size(G, 0)
+max_iters = 30
+
+# generate data
+noise = 0.1
+w = np.reshape(logo, [np.size(logo), 1])
+codeWord = np.dot(G.astype(float), w) % 2
+noisyCodeWord = channel_noise(codeWord, noise)
+F = init_ldpc_graph(H, noisyCodeWord, 1 - noise)
+actualError = np.sum(noisyCodeWord != codeWord) / len(noisyCodeWord)
+print('actual error: ', actualError)
+nodeMarg_par = run_loopy_bp_parallel(F, max_iters, conv_tol)
+iters = len(nodeMarg_par)
+if iters < max_iters:
+    print('Parallel LBP converged in', iters, 'iterations.')
+else:
+    print('Parallel LBP did not converge. Terminated at', max_iters, 'iterations')
+
+plotAtIters = [0, 1, 2, 3, 5, 10, 20, 30]
+plt.figure(5)
+fig, axs = plt.subplots(2, 4, constrained_layout=True, subplot_kw={'xticks': [], 'yticks': []})
+for index in range(len(plotAtIters)):
+    i = plotAtIters[index]
+
+    if i == 0:
+        thisX = noisyCodeWord
+    elif i > len(nodeMarg_par):
+        thisX = estimate_code(nodeMarg_par[-1])
+    else:
+        thisX = estimate_code(nodeMarg_par[i])
+
+    msg = np.reshape(thisX[:1600], [40, 40])
+    par = np.reshape(thisX[1600:], [40, 40])
+    im = np.vstack([msg, par])
+    axs.flat[index].imshow(im, cmap='gray')
+    axs.flat[index].set_xlabel('Iter ' + str(i))
+
+
+
+
